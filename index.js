4c9991cb021874d202ede70f942aacb278644059
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2022-06-11 11:32:43 +0200

    return file and dir names using Promise.all

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const util = require('util');
+const lstat = util.promisify(fs.lstat);
+// const { lstat } = fs.promises;    is another version of the above lstat
 const { cwd } = require('process');
 // or 
 // const process = require('process');
-// and place the below cwd() with process.cwd() and this should work too
+// and replace the below cwd() with process.cwd() and this should work too
 
-fs.readdir(cwd(), (err, fileNames) => {
+fs.readdir(cwd(), async (err, fileNames) => {
     if (err) {
         throw new Error(err);
     }
 
-    console.log(fileNames); 
-})
+    const statPromises = fileNames.map(fileName => {
+        return lstat(fileName);
+    })
+
+    const readyStats = await Promise.all(statPromises);
+
+    for (let stat of readyStats) {
+        const index = readyStats.indexOf(stat);
+
+        console.log(fileNames[index], stat.isFile());
+    }
+    
+});
